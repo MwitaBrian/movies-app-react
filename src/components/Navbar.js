50ecb6790ad9c4c6d26e51943414e7bf0fd5db73
MwitaBrian <brianmwita13@gmail.com>
2022-10-09 04:09:46 +0300

    Add navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,21 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Action</a></li>
-        <li><a href="#">Drama</a></li>
-        <li><a href="#">Comedy</a></li>
-        <li><a href="#">Kids</a></li>
-      </ul>
-      <form>
-        <div className='search-btn'>
-          <input type="text" placeholder='Search Movie Name' className='inputText' />
-          <button><i class="fa-solid fa-magnifying-glass"></i></button>
-        </div>
-      </form>
+      <div className='header'>
+        <ul className='navbar-ul'>
+          <li className='navbar-ul-li'><a href="#" className='navbar-ul-li-a'>Home</a></li>
+          <li className='navbar-ul-li'><a href="#" className='navbar-ul-li-a'>Action</a></li>
+          <li className='navbar-ul-li'><a href="#" className='navbar-ul-li-a'>Drama</a></li>
+          <li className='navbar-ul-li'><a href="#" className='navbar-ul-li-a'>Comedy</a></li>
+          <li className='navbar-ul-li'><a href="#" className='navbar-ul-li-a'>Kids</a></li>
+        </ul>
+        <form>
+          <div className='search-btn'>
+            <input type="text" placeholder='Search Movie Name' className='inputText' />
+            <button><i class="fa-solid fa-magnifying-glass"></i></button>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
