dfc3a5497d7d16676236dc0c2fa9d34aa87277bf
MwitaBrian <brianmwita13@gmail.com>
2022-10-09 13:47:59 +0300

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Card from './components/Card';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Card />
+      <Navbar />
+      <Home />
     </>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+
+export default function Home() {
+  return (
+    <>
+      <div className='container'>
+        <Card />
+      </div>
+    </>
+  )
+}
